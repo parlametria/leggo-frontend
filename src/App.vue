402db0d4a47c02f5a988e345860a3dd685466e9d
Daniel Rodrigues Coura <danielrcifpb@gmail.com>
2018-08-30 09:17:39 -0300

    Corrige pagina home

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       background-color="#545c64"
       text-color="#fff"
       active-text-color="#ffd04b">
-      <el-menu-item index="home">Home</el-menu-item>
+      <el-menu-item index="/">Home</el-menu-item>
       <el-menu-item index="proposicoes">Proposições</el-menu-item>
       <el-menu-item index="about">About</el-menu-item>
     </el-menu>
