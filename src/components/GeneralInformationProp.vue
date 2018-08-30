e6c3d99bb9f809a1d8fb83cf4928122465e8b67a
Daniel Rodrigues Coura <danielrcifpb@gmail.com>
2018-08-30 08:46:08 -0300

    Remove classes do tr

--- src/components/GeneralInformationProp.vue
+++ src/components/GeneralInformationProp.vue
@@ -5,39 +5,39 @@
         </div>
 
         <table style="width:100%">
-            <tr class="text item">
+            <tr>
                 <td><b>Autor</b></td>
                 <td>{{ proposicao.autor }}</td>
             </tr>
-            <tr class="text item">
+            <tr>
                 <td><b>Casa de Origem</b></td>
                 <td>{{ proposicao.casa_origem }}</td>
             </tr>
-            <tr class="text item">
+            <tr>
                 <td><b>Data de Apresentação</b></td>
                 <td>{{ proposicao.data_apresentacao }}</td>
             </tr>
-            <tr class="text item">
+            <tr>
                 <td><b>Ementa</b></td>
                 <td>{{ proposicao.ementa }}</td>
             </tr>
-            <tr class="text item">
+            <tr>
                 <td><b>Explicação da Ementa</b></td>
                 <td>{{ proposicao.explicacao_ementa }}</td>
             </tr>
-            <tr class="text item">
+            <tr>
                 <td><b>Status Atual</b></td>
                 <td>{{ proposicao.status_atual }}</td>
             </tr>
-            <tr class="text item">
+            <tr>
                 <td><b>Forma de Apreciação</b></td>
                 <td>{{ proposicao.forma_apreciacao }}</td>
             </tr>
-            <tr class="text item">
+            <tr>
                 <td><b>Regime de Tramitação</b></td>
                 <td>{{ proposicao.regime_tramitacao }}</td>
             </tr>
-            <tr class="text item">
+            <tr>
                 <td><b>Último Relator</b></td>
                 <td>{{ proposicao.ultimo_relator }}</td>
             </tr>
