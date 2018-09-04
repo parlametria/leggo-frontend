bf756c92eb4845af5e7414eb564d8a4b1ed35210
Juan Barros <juanlyrabarros@gmail.com>
2018-09-04 16:18:27 -0300

    Testa tela de detalhamento de lei 91341

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,4 +3,8 @@ describe('My First Test', () => {
     cy.visit('http://localhost:8080/#/proposicoes')
     cy.contains('h1', 'Proposições')
   })
+
+  it('Visits the page of 91341', () => {
+    cy.visit('http://localhost:8080/#/proposicoes/senado/91341')
+  })
 })
