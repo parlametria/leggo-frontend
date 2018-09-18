cfe232d9f53d7b5832caa5f3a6004760cf657fbc
thayannevls <thayannevls@gmail.com>
2018-09-18 11:28:53 -0300

    remove componente

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,6 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('http://localhost:8080/#/proposicoes')
-    cy.contains('h1', 'Proposições')
   })
 
   it('Visits the page of 91341', () => {
