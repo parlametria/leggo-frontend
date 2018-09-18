d6b88bab05cff3e49a29a739bcf8c501cb2fd90f
Juan Barros <juanlyrabarros@gmail.com>
2018-09-18 13:37:17 -0300

    Adiciona teste de FasesBar

--- tests/unit/FasesBar.spec.js
+++ tests/unit/FasesBar.spec.js
@@ -1,21 +1,25 @@
 import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import FasesBar from '@/components/FasesBar.vue'
 
 describe('FasesBar', () => {
-  it('renders fasesbar when all fields are passed', () => {
+    let cmp;
     const fases = [
         {'data_inicio': '2018-09-16', 'data_fim': '2018-09-17', 'casa': 'senado', 'nome': 'Comissões'},
         {'data_inicio': '2018-09-17', 'data_fim': '2018-09-17', 'casa': 'senado', 'nome': 'Plenário'},
         {'data_inicio': '2018-09-17', 'data_fim': '2018-09-19', 'casa': 'camara', 'nome': 'Comissões'},
         {'data_inicio': '2018-09-19', 'data_fim': '2018-10-19', 'casa': 'camara', 'nome': 'Plenário'}
     ]
-    const wrapper = shallowMount(FasesBar, {
-        propsData: { fases }
-    })
 
-    fases.forEach(fase => {
-      expect(wrapper.text()).to.include(fase.nome)
-    })
-  })
+    beforeEach(() => {
+        cmp = mount(FasesBar, {
+            propsData: { fases }
+        })
+    });
+
+    it('renders fasesbar when all fields are passed', () => {
+        expect(cmp.contains('.fase')).to.be.true
+        expect(cmp.findAll('.fase')).to.have.lengthOf(fases.length)
+    }) 
+    
 })
