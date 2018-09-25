544f75c91653110386a9749bbf40f8b7be0a7c8f
Jair Guedes <jairguedesferreira@gmail.com>
2018-09-25 14:14:26 -0300

    Testa filtro de apreciacao e regime

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,6 +1,30 @@
 describe('My First Test', () => {
-  it('Visits the app root url', () => {
+
+  it('Test filter apreciacao', () => {
     cy.visit('http://localhost:8080/#/proposicoes')
+    const apreciacoes = ['Plenário', 'Conclusiva']
+      cy.contains('Apreciação').click()
+      apreciacoes.forEach(apreciacao => {
+        cy.get('#tags > div > span').should('contain', apreciacao)
+        cy.contains('.is-checked', apreciacao).click()
+        cy.get('#tags > div > span').should('not.contain', apreciacao)
+        cy.contains('span', apreciacao).click()
+        cy.get('#tags > div > span').should('contain', apreciacao)
+      });
+  })
+
+  it('Test filter regime', () => {
+    cy.visit('http://localhost:8080/#/proposicoes')
+
+    const regimes = ['Urgência', 'Ordinária', 'Prioridade']
+    cy.contains('Regime de tramitação').click()
+    regimes.forEach(regime => {
+      cy.get('#tags > div > span').should('contain', regime)
+      cy.contains('.is-checked', regime).click()
+      cy.get('#tags > div > span').should('not.contain', regime)
+      cy.contains('span', regime).click()
+      cy.get('#tags > div > span').should('contain', regime)
+    });
   })
 
   it('Visits the page of 91341', () => {
