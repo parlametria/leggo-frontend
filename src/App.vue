0dc20ba051c917965f4ff30301e4aab38da652d6
Tarciso Oliveira <tarcisocomp@gmail.com>
2018-09-26 18:35:09 -0300

    Atualiza página do Sobre com links para explicação da energia e para repositórios do projeto.

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       active-text-color="#ffd04b">
       <el-menu-item index="/">Home</el-menu-item>
       <el-menu-item index="/proposicoes">Proposições</el-menu-item>
-      <el-menu-item index="/about">About</el-menu-item>
+      <el-menu-item index="/sobre">Sobre</el-menu-item>
     </el-menu>
 
     <el-container>
