53c73c94b7805ee9b5bcb2cd895138b3b75d2e63
AndresMRM <andres@inventati.org>
2018-10-03 10:58:19 -0300

    modifica html sobre

--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="sobre" align="left">
     <h1>Sobre</h1>
-    <a href="https://analytics-ufcg.github.io/agora-digital/public/articles/tutorial_4_relatorio_energia.html">Entenda a Energia de um PL</a>
-    <h1>Contribua com o Ágora Digital</h1>
-    <a href="https://github.com/analytics-ufcg/agora-digital">Pacote R</a>
-    <br>
-    <br>
-    <a href="https://github.com/analytics-ufcg/agora-digital-web">Ágora-Web</a>
-    <br>
-    <br>
-    <a href="https://github.com/analytics-ufcg/agora-digital-backend">Ágora-Backend</a>
+    <ul>
+      <li>
+        <a href="https://analytics-ufcg.github.io/agora-digital/public/articles/tutorial_4_relatorio_energia.html">Entenda a Energia de um PL</a>
+      </li>
+    </ul>
+    <h2>Contribua com o Ágora Digital</h2>
+    <ul>
+      <li>
+        <a href="https://github.com/analytics-ufcg/agora-digital">Pacote R</a>
+      </li>
+      <li>
+        <a href="https://github.com/analytics-ufcg/agora-digital-web">Ágora-Web</a>
+      </li>
+      <li>
+        <a href="https://github.com/analytics-ufcg/agora-digital-backend">Ágora-Backend</a>
+      </li>
+    </ul>
   </div>
 </template>
