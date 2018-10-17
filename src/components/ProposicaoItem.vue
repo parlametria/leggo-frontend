1600d4ee0177c8966d8c8c376371d2cf0a81290e
Daniel Rodrigues Coura <danielrcifpb@gmail.com>
2018-10-17 11:39:01 -0300

    Conserta sobreposição do gráfico e da timeline

--- src/components/ProposicaoItem.vue
+++ src/components/ProposicaoItem.vue
@@ -7,8 +7,8 @@
     <div class="collapse-box">
       <el-card shadow="hover" class="box-card prop-item" :class="{ 'border-pauta': this.prop.em_pauta }">
         <el-row>
-          <el-col :sm="2" :md="6" :lg="6"><fases-bar :fases="prop.resumo_progresso"/></el-col>
-          <el-col :sm="2" :md="6" :lg="6"><energy-graphic :date="dateRef" :visId="visId" :id="prop.id_ext" :casa="prop.casa"/></el-col>
+          <el-col :sm="12" :md="6" :lg="6"><fases-bar :fases="prop.resumo_progresso"/></el-col>
+          <el-col :sm="12" :md="6" :lg="6"><energy-graphic :date="dateRef" :visId="visId" :id="prop.id_ext" :casa="prop.casa"/></el-col>
         </el-row>
         <a class="sigla" :href="prop.url">
           {{ prop.sigla }}
