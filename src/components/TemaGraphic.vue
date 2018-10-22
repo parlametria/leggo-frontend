8d54b40c49afba9e5b363c9a8f4c15e683fb21dd
Juan Barros <juanlyrabarros@gmail.com>
2018-10-22 14:26:00 -0300

    Retira Warnings de vega lite

--- src/components/TemaGraphic.vue
+++ src/components/TemaGraphic.vue
@@ -9,110 +9,110 @@ import { mapState } from 'vuex'
 export default {
   name: 'TemaGraphic',
   props: {
-      tema: String
+    tema: String
   },
-  data() {
+  data () {
     return {
-        windowWidth: window.innerWidth
+      windowWidth: window.innerWidth
     }
   },
-  async mounted(){
-      this.criaGrafico()
-      window.onresize = () => {
-        this.windowWidth = window.innerWidth
-      }
+  async mounted () {
+    this.criaGrafico()
+    window.onresize = () => {
+      this.windowWidth = window.innerWidth
+    }
   },
   methods: {
-    criaGrafico() {
-        vegaEmbed(`#temaGrafico`, this.graphJson)
+    criaGrafico () {
+      vegaEmbed(`#temaGrafico`, this.graphJson)
     }
   },
   computed: {
-    graphJson() {
-        return {
-            $schema: "https://vega.github.io/schema/vega-lite/v2.json",
-            description: "Area chart showing weight of cars over time.",
-            width: this.width * 0.8, 
-            height: 300,
-            data: { values: this.formatEnergias },
-            mark: "area",
-            config: {
-                autosize: {
-                    type: "fit",
-                    contains: "padding"
-                },
-                legend: {
-                    orient: this.width <= 990 ? "bottom": "right"
-                }
-            },
-            encoding: {
-                x: {
-                    field: "periodo",
-                    type: "temporal",
-                    axis: { title: "Dia" }
-                    },
-                y: {
-                    aggregate: "sum", 
-                    field: "energia_recente",
-                    type: "quantitative",
-                    axis: { title: "Energia", "titlePadding": -7  }
-                    },
-                color: {
-                    field: "apelido",
-                    type: "nominal",
-                    axis: { title: "Apelido" },
-                    scale: {
-                        scheme: "accent"
-                    }
-                }
+    graphJson () {
+      return {
+        $schema: 'https://vega.github.io/schema/vega-lite/v2.json',
+        description: 'Area chart showing weight of cars over time.',
+        width: this.width * 0.8,
+        height: 300,
+        data: { values: this.formatEnergias },
+        mark: 'area',
+        config: {
+          autosize: {
+            type: 'fit',
+            contains: 'padding'
+          },
+          legend: {
+            orient: this.width <= 990 ? 'bottom' : 'right'
+          }
+        },
+        encoding: {
+          x: {
+            field: 'periodo',
+            type: 'temporal',
+            axis: { title: 'Dia' }
+          },
+          y: {
+            aggregate: 'sum',
+            field: 'energia_recente',
+            type: 'quantitative',
+            axis: { title: 'Energia', 'titlePadding': -7 }
+          },
+          color: {
+            field: 'apelido',
+            type: 'nominal',
+            axis: { title: 'Apelido' },
+            scale: {
+              scheme: 'accent'
             }
+          }
         }
+      }
     },
-    width() {
-        let result = this.windowWidth
-        if(result >= 1920){
-            result -= 200
-        } else if (result >= 990){
-            result -= 300 
-        } else if (result >= 768){
-            result -= 280 
-        }else {
-            result -= 0
+    width () {
+      let result = this.windowWidth
+      if (result >= 1920) {
+        result -= 200
+      } else if (result >= 990) {
+        result -= 300
+      } else if (result >= 768) {
+        result -= 280
+      } else {
+        result -= 0
+      }
+      return result
+    },
+    formatEnergias () {
+      let result = []
+      this.proposicoes.filter((prop) => prop.tema === this.tema).forEach((prop) => {
+        const energias = this.energias[prop.lastEtapa.id_ext]
+        if (energias) {
+          energias.forEach((energia) => {
+            result.push({ ...energia, apelido: prop.lastEtapa.apelido })
+          })
         }
-        return result
-    },
-    formatEnergias() {
-        let result = []
-        this.proposicoes.filter((prop) => prop.tema === this.tema).forEach((prop) => {
-            const energias = this.energias[prop.lastEtapa.id_ext]
-            if(energias){
-                energias.forEach((energia)=> {
-                    result.push({ ...energia, apelido: prop.lastEtapa.apelido })
-                })
-            }   
-        })
-        return result
+      })
+      return result
     },
     ...mapState({
-        energias: state => state.proposicoes.energias,
-        proposicoes: state => state.proposicoes.proposicoes
+      energias: state => state.proposicoes.energias,
+      proposicoes: state => state.proposicoes.proposicoes
     })
   },
   watch: {
     energias: {
-      handler(val, oldVal) {
+      handler (val, oldVal) {
         this.criaGrafico()
       },
       deep: true
     },
     proposicoes: {
-      handler(val, oldVal) {
+      handler (val, oldVal) {
         this.criaGrafico()
       },
       deep: true
     },
     windowWidth: {
-      handler(val, oldVal) {
+      handler (val, oldVal) {
         this.criaGrafico()
       },
       deep: true
