4cb49e676c8ce6b4f60f6d3ebf4cb5a8828fd85b
AndresMRM <andres@inventati.org>
2018-10-29 14:17:20 -0300

    remove css desnecessário

--- src/components/FasesBar.vue
+++ src/components/FasesBar.vue
@@ -64,20 +64,9 @@ export default {
     background-color: #EEE;
     margin-right: 3px;
 }
-.fase:hover{
-  .tooltip{
-    visibility: visible;
-  }
-}
-.fase:last-child {
-
-}
 .cerrilhado {
     background: repeating-linear-gradient(-45deg, white, white 2px, tomato 0, tomato 4px);
     display: block;
-}
-.fase .tooltip {
-  visibility: hidden;
 }
 .faseConcluida.faseSenado{
     background-color: #a6cee3;
@@ -90,40 +79,6 @@ export default {
 }
 .faseAtual.faseCamara{
     background-color: #33a02c;
-}
-.triangulo {
-    width: 0;
-    height: 0;
-    margin-top: -8px;
-    margin-left: 20px;
-    border-left: 3px solid transparent;
-    border-right: 3px solid transparent;
-    border-top: 6px solid rgb(255, 20, 20);
-}
-.tooltiptriangulo{
-    width: 0;
-    height: 0;
-    position: inherit;
-    margin-top: -35px;
-    margin-left: -10px;
-    border-right: 5px solid rgba(77, 76, 128, 0.7);
-    border-bottom: 5px solid transparent;
-    border-top: 5px solid transparent;
-}
-.tooltip {
-  border-radius: 6px;
-  position: absolute;
-  text-align: center;
-  width: auto;
-  height: auto;
-  background-color: rgba(77, 76, 128, 0.7);
-  color: #FFF;
-  pointer-events: none;
-  padding: 5px;
-  z-index: 1000;
-  float: right;
-  margin-top: -20px;
-  margin-left: 40px;
 }
 .arrow-down {
   margin-left: 10px;
