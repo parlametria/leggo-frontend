e4864e886049d5f9e36434621315de91f68349a0
AndresMRM <andres@inventati.org>
2018-11-07 07:09:59 -0200

    corrige lint

--- src/components/PressureBar.vue
+++ src/components/PressureBar.vue
@@ -8,23 +8,23 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import { mapGetters } from 'vuex'
 
 export default {
-  name: "PressureBar",
+  name: 'PressureBar',
   props: ['energia'],
   methods: {
-    pegaPorcentagem() {
-      return (this.energia * 100) / this.maxPressao;
+    pegaPorcentagem () {
+      return (this.energia * 100) / this.maxPressao
     },
-    retornaEstilo() {
-      return `width:${this.pegaPorcentagem()}%`;
+    retornaEstilo () {
+      return `width:${this.pegaPorcentagem()}%`
     }
   },
   computed: {
     ...mapGetters(['maxPressao'])
-    }
-};
+  }
+}
 </script>
 
 <style scoped>
