33269dea2fa05a7118ed47329be415a53dfd1900
Jefferson Neves <jefferson.rpn@gmail.com>
2018-11-13 18:23:06 -0300

    Centraliza a logo e as proposições

--- src/views/Proposicoes.vue
+++ src/views/Proposicoes.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="content">
-    <div class="middle">
-      <img src="../assets/logoweb.png" class="logo" />
-    <p v-if="pending.proposicoes">loading posts...</p>
-    <p v-if="error.proposicoes">loading failed</p>
-    <transition name="el-fade-in" mode="out-in">
-      <div v-if="filteredProps.length">
-        <transition-group name="el-fade-in" tag="div">
-          <proposicao-item :key="prop.apelido" v-for="prop in filteredProps" :prop="prop"/>
-        </transition-group>
-      </div>
-      <p v-else>Nenhuma proposição para mostrar...</p>
-    </transition>
-    </div>
+    <el-row type="flex" justify="space-around" class="logo-container">
+      <el-col :xs="24" :sm="18" :md="12" :lg="12" :xl="8">
+        <img src="../assets/logoweb.png" class="logo" width="100%" />
+      </el-col>
+    </el-row>
+    <el-row type="flex" justify="space-around">
+      <el-col :xs="24" :sm="18" :md="12" :lg="12" :xl="8">
+      <p v-if="pending.proposicoes">loading posts...</p>
+      <p v-if="error.proposicoes">loading failed</p>
+      <transition name="el-fade-in" mode="out-in">
+        <div v-if="filteredProps.length">
+          <transition-group name="el-fade-in" tag="div">
+            <proposicao-item :key="prop.apelido" v-for="prop in filteredProps" :prop="prop"/>
+          </transition-group>
+        </div>
+        <p v-else>Nenhuma proposição para mostrar...</p>
+      </transition>
+      </el-col>
+    </el-row>
     
   </div>
 </template>
@@ -89,11 +95,12 @@ export default {
    margin:auto;
 }
 
-.middle {
-  
+.logo-container {
+  background-color: #000000;
+  margin-bottom: 2rem;
 }
 
-.logo{
+.logo {
   max-width: 100%;
   height: auto;
 }
