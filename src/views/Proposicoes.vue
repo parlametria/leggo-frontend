c159abe37db76dd623071b9b08631329a492f883
Jair Guedes <jairguedesferreira@gmail.com>
2018-11-29 17:08:29 -0300

    Ajeita lint

--- src/views/Proposicoes.vue
+++ src/views/Proposicoes.vue
@@ -84,14 +84,14 @@ export default {
         filter => this.filter.current[filter].includes(prop[filter])
       )
     },
-        dataAtualFormatada(){
-    var data = new Date(),
-        dia  = data.getDate().toString(),
-        diaF = (dia.length == 1) ? '0'+dia : dia,
-        mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
-        mesF = (mes.length == 1) ? '0'+mes : mes,
-        anoF = data.getFullYear();
-    return diaF+"/"+mesF+"/"+anoF;
+    dataAtualFormatada () {
+      const data = new Date()
+      const dia = data.getDate().toString()
+      const diaF = (dia.length === 1) ? '0' + dia : dia
+      const mes = (data.getMonth() + 1).toString() // +1 pois no getMonth Janeiro começa com zero.
+      const mesF = (mes.length === 1) ? '0' + mes : mes
+      const anoF = data.getFullYear()
+      return diaF + '/' + mesF + '/' + anoF
     },
     checkPautaFilter (prop) {
       return this.filter.emPautaFilter.some(options => {
