dddf49ba44e37dda0b0b142a4f80109075eb2893
AndresMRM <andres@inventati.org>
2018-12-06 15:34:47 -0200

    arruma barra de progresso quando tem Revisão II

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
       <el-footer class="footer">
         <p>
           <span v-if="commitHash">versão <a :href="`https://github.com/analytics-ufcg/agora-digital-frontend/commit/${commitHash}`" target="_blank">{{ commitHash }}</a></span>
-          <span v-if="buildDate">compilada em {{ buildDate }}</span>
+          <span v-if="buildDate"> compilada em {{ buildDate }}</span>
         </p>
       </el-footer>
     </el-container>
