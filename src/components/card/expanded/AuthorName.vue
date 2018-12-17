9e668c22821b83882fe13bada4ea5e19d19ba3a1
Thayanne <thayannevls@gmail.com>
2018-12-17 17:02:35 -0300

    bugfix quando tem mtos relatores

---
+++ src/components/card/expanded/AuthorName.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <el-popover 
+            v-if="containsMoreThanOneAuthor()"
+            class="item" 
+            width="200"
+            placement="right-start"
+            trigger="hover">
+                <span slot="reference" class="tooltip">Vários...</span>
+                Autores <br/>
+                <span class="authors">{{format(normalizedAuthor)}}</span>
+         </el-popover>
+        <div v-else class="author">
+            {{normalizedAuthor}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AuthorName',
+    props: {
+        author: {
+            type: String,
+            default: 'Autor não encontrado',
+            validator: value => {
+                return value != null
+            }
+        } 
+    },
+    computed: {
+        normalizedAuthor () {
+            return this.removeCasa(this.author)
+        }
+    },
+    methods: {
+        removeCasa(author){
+            author = author.split(' - ')
+            return author.pop()
+        },
+        containsMoreThanOneAuthor(){
+            if(this.normalizedAuthor.includes(', '))
+                return true
+            return false
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.authors{
+  white-space: pre-line;
+  word-spacing: 1px;
+  font-size: 12px;  
+}
+.author {
+    font-size: 15px;
+}
+.tooltip:hover {
+    text-decoration: underline;
+}
+</style>
+
