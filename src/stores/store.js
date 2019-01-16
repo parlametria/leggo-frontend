311d1809c8d4f5c8ab2dc7b2f5674feb2a29b7dc
gileadekelvin <gileadekelvin@gmail.com>
2019-01-16 11:49:02 -0300

    Refatora store de proposições Cria um novo arquivo para conter a store de proposições

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Vapi from 'vuex-rest-api'
+import proposicoesStore from './proposicoes'
 import filterStore from './filter'
 import pautasStore from './pautas'
 import temperaturasStore from './temperaturas'
@@ -9,50 +9,9 @@ import emendasStore from './emendas'
 
 Vue.use(Vuex)
 
-const proposicoes = new Vapi({
-  baseURL: process.env.VUE_APP_API_URL,
-  state: {
-    proposicoes: [],
-    tramitacoes: new Set(),
-    pautas: {},
-    eventos_tramitacao: {},
-    metaInfo: {}
-  }
-}).get({
-  action: 'listProposicoes',
-  path: '/proposicoes',
-  onSuccess: (state, { data }) => {
-    state.proposicoes = data
-    data.forEach((prop) => {
-      // TODO: por enquanto usa apenas a última etapa
-      prop.lastEtapa = prop.etapas.slice(-1)[0]
-    })
-  }
-}).get({
-  action: 'getMetaInfo',
-  path: '/info',
-  property: 'metaInfo'
-}).getStore()
-
-proposicoes.getters = {
-  perFilterOptions (state) {
-    // Retorna um obj com todas as opções de valores para cada filtro, baseado
-    // nos dados das proposições
-    let options = {}
-    for (let filter of filterStore.state.filters) {
-      // O Set aqui é usado para deixar só os valores distintos
-      options[filter] = [...new Set(
-        // Pega, em cada proposição, o valor do atributo ao qual o filtro se refere
-        state.proposicoes.map(p => p.lastEtapa[filter])
-      ).values()]
-    }
-    return options
-  }
-}
-
 export default new Vuex.Store({
   modules: {
-    proposicoes,
+    proposicoes: proposicoesStore,
     filter: filterStore,
     pautas: pautasStore,
     temperaturas: temperaturasStore,
