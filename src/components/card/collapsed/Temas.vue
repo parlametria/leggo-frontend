768244384744464c26b77a009f6dd4c9981840ed
Ivyna Santino <ivyna@regressao.analytics.ufcg.edu.br>
2019-03-29 17:13:17 -0300

    Adiciona tags para temas das proposições

---
+++ src/components/card/collapsed/Temas.vue
@@ -0,0 +1,28 @@
+<template>
+    <div  class="tagTemas">
+       <div v-for="(tema, i) in temas" :key="i">
+           <span class="tag">{{ tema }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Temas",
+    props: {
+        temas: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.tagTemas {
+    display: flex;
+    flex-direction: row;
+}
+.tag {
+    background-color: #FFA07A;
+    margin-right: 10px;
+    border-radius: 5%;
+}
+</style>
