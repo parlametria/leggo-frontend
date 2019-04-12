21d35865903e9b1d597dd22fb4ac719356f83b84
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-04-12 15:55:14 -0300

    Mostra título da emenda na tabela de emendas. Reordena colunas para visualização mais objetiva.

--- src/components/card/expanded/EmendasTabContent.vue
+++ src/components/card/expanded/EmendasTabContent.vue
@@ -4,27 +4,25 @@
     stripe
     style="width: 100%">
     <el-table-column
+      label="Emenda">
+      <template slot-scope="scope">
+        <a :href="scope.row.inteiro_teor" target="_blank">{{ scope.row.titulo }}</a>
+      </template>
+    </el-table-column>
+    <el-table-column
       prop="distancia"
       label="Coerência">
       <template slot-scope="scope">
       {{ formatDistancia(scope.row.distancia) }}
       </template>
     </el-table-column>
-
-    <el-table-column
-      prop="local"
-      label="Local">
-    </el-table-column>
     <el-table-column
       prop="autor"
       label="Autor">
     </el-table-column>
     <el-table-column
-      prop="numero"
-      label="Numero">
-      <template slot-scope="scope">
-        <a :href="scope.row.inteiro_teor" target="_blank">{{ scope.row.numero }}</a>
-      </template>
+      prop="local"
+      label="Local">
     </el-table-column>
   </el-table>
 </template>
