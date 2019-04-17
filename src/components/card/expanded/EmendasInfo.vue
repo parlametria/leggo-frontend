d3b0b7dc962ff5a3b9edf0e99d9fd635d883b1ed
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-04-17 17:28:17 -0300

    Corrige operador de comparação para verificar se dado de distância está faltando.

--- src/components/card/expanded/EmendasInfo.vue
+++ src/components/card/expanded/EmendasInfo.vue
@@ -72,7 +72,7 @@ export default {
     },
     verificaSeMostraEmendas () {
       if (this.propEmendas && this.propEmendas.length) {
-        return this.orderedEmendas[0].distancia != -1
+        return this.orderedEmendas[0].distancia !== -1
       } else {
         return false
       }
