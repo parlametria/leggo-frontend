99a77f7bf3824ff0ea7360d11b87556043799db0
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-04-22 16:24:17 -0300

    Muda texto explicativo da categoria das emendas para o componente EmendasTabContent, alterando a quantidade de emendas e a categoria dinamicamente.

--- src/components/card/expanded/EmendasTabContent.vue
+++ src/components/card/expanded/EmendasTabContent.vue
@@ -1,25 +1,28 @@
 <template>
-  <el-table
-    :data="emendas"
-    stripe
-    style="width: 100%">
-    <el-table-column
-      label="Emenda">
-      <template slot-scope="scope">
-        <a :href="scope.row.inteiro_teor+'&disposition=inline'" target="_blank">{{ scope.row.titulo }}</a>
-      </template>
-    </el-table-column>
-    <el-table-column
-      label="Autor">
-      <template slot-scope="scope">
-      {{ corrigePartidoAutor(scope.row.autor) }}
-      </template>
-    </el-table-column>
-    <el-table-column
-      prop="local"
-      label="Local">
-    </el-table-column>
-  </el-table>
+  <div>
+    <span>Top-{{ emendas.length }} Emendas cujas propostas de mudança do texto original são <span style="color:#dc6060">Mais {{categoria}}</span>.</span>
+    <el-table
+      :data="emendas"
+      stripe
+      style="width: 100%">
+      <el-table-column
+        label="Emenda">
+        <template slot-scope="scope">
+          <a :href="scope.row.inteiro_teor+'&disposition=inline'" target="_blank">{{ scope.row.titulo }}</a>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="Autor">
+        <template slot-scope="scope">
+        {{ corrigePartidoAutor(scope.row.autor) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="local"
+        label="Local">
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -27,7 +30,8 @@
 export default {
   name: 'EmendasTabContent',
   props: {
-    emendas: Array
+    emendas: Array,
+    categoria: String
   },
   methods: {
     corrigePartidoAutor (autor) {
