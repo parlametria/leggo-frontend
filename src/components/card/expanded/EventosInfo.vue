b45b6c46488705c058cc03668b05c7e9ccecd449
Jair Guedes <jairguedesferreira@gmail.com>
2019-05-20 13:15:30 -0300

    Agrupa últimos eventos

--- src/components/card/expanded/EventosInfo.vue
+++ src/components/card/expanded/EventosInfo.vue
@@ -1,28 +1,32 @@
 <template>
-    <el-collapse v-if="formattedEventos.length" v-model="activeNames">
-      <el-collapse-item name="1">
-        <template slot="title">
-          <span class="title">Últimos Eventos</span>
-        </template>
-        <table class="eventos-tram">
-          <tr v-for="(evento, index) in formattedEventos" :key="index">
-            <td class="date-field">
-              <el-tooltip :content="evento.data" placement="bottom">
-                <div>{{evento.dataDiff}}</div>
-              </el-tooltip>
-              <div class="sigla-local">{{evento.sigla}}</div>
-            </td>
-            <td>
-              <div class="evento-title">{{evento.title}}</div>
-              <div :class="{clickable: evento.collapsible}"
-                   @click="toggleCollapseDescription(index)">
-                {{evento.texto}}
-              </div>
-            </td>
-          </tr>
-        </table>
-      </el-collapse-item>
-    </el-collapse>
+  <el-collapse v-if="formattedEventos.length" v-model="activeNames">
+    <el-collapse-item name="1">
+      <template slot="title">
+        <span class="title">Últimos Eventos</span>
+      </template>
+      <table class="eventos-tram">
+        <div v-for="(eventos, key) in groupEventos" :key="key">
+          <td class="date-field">
+            <el-tooltip :content="eventos['evento'][0].data" placement="bottom">
+              <div>{{eventos['evento'][0].dataDiff}}</div>
+            </el-tooltip>
+            <div class="sigla-local">{{eventos['evento'][0].sigla}}</div>
+          </td>
+          <td>
+            <div
+              v-if="eventos['evento'].length > 1"
+              class="evento-title"
+            >{{eventos['evento'].length}} eventos de {{eventos['evento'][0].title}}</div>
+            <div v-else class="evento-title">{{eventos['evento'][0].title}}</div>
+            <div
+              :class="{clickable: eventos['evento'][0].collapsible}"
+              @click="toggleCollapseDescription(key)"
+            >{{eventos['evento'][0].texto}}</div>
+          </td>
+        </div>
+      </table>
+    </el-collapse-item>
+  </el-collapse>
 </template>
 
 <script>
@@ -55,30 +59,40 @@ export default {
   },
   computed: {
     formattedEventos () {
-      return (this.eventosTramitacao[this.id] || []).map((eventoTram, index) => {
-        return {
-          data: this.formatDate(eventoTram.data),
-          dataDiff: this.formatDateDifference(eventoTram.data),
-          sigla: eventoTram.sigla_local === 'nan' ? '' : eventoTram.sigla_local,
-          title: eventoTram.titulo_evento,
-          texto: this.formatTextoTramitacao(eventoTram.texto_tramitacao, index),
-          collapsible: eventoTram.texto_tramitacao.length > this.MAX_TEXT_LENGTH
+      return (this.eventosTramitacao[this.id] || []).map(
+        (eventoTram, index) => {
+          return {
+            data: this.formatDate(eventoTram.data),
+            dataDiff: this.formatDateDifference(eventoTram.data),
+            sigla:
+              eventoTram.sigla_local === 'nan' ? '' : eventoTram.sigla_local,
+            title: eventoTram.titulo_evento,
+            texto: this.formatTextoTramitacao(
+              eventoTram.texto_tramitacao,
+              index
+            ),
+            collapsible:
+              eventoTram.texto_tramitacao.length > this.MAX_TEXT_LENGTH
+          }
         }
-      })
+      )
     },
     groupEventos () {
-      var groups = {};
+      var groups = {}
       for (var i = 0; i < this.formattedEventos.length; i++) {
-        var groupName = this.formattedEventos[i].data.concat(this.formattedEventos[i].title);
+        var groupName = this.formattedEventos[i].data.concat(
+          this.formattedEventos[i].title
+        )
         if (!groups[groupName]) {
-          groups[groupName] = [];
+          groups[groupName] = []
         }
-        groups[groupName].push(this.formattedEventos[i]);
+        groups[groupName].push(this.formattedEventos[i])
       }
-      myArray = [];
-      for (var groupName in groups) {
-        myArray.push({group: groupName, evento: groups[groupName]});
+      var myArray = []
+      for (groupName in groups) {
+        myArray.push({ group: groupName, evento: groups[groupName] })
       }
+      return myArray
     },
     ...mapState({
       eventosTramitacao: state => state.eventosTramitacao.eventosDict
@@ -103,10 +117,16 @@ export default {
 
       if (differenceInDays > 365) {
         const differenceInYears = Math.floor(differenceInDays / 365)
-        dateInTextFormat = differenceInYears === 1 ? 'Há ± 1 ano' : `Há ± ${differenceInYears} anos`
+        dateInTextFormat =
+          differenceInYears === 1
+            ? 'Há ± 1 ano'
+            : `Há ± ${differenceInYears} anos`
       } else if (differenceInDays > 30) {
         const differenceInMonths = Math.floor(differenceInDays / 30)
-        dateInTextFormat = differenceInMonths === 1 ? 'Há ± 1 mês' : `Há ± ${differenceInMonths} meses`
+        dateInTextFormat =
+          differenceInMonths === 1
+            ? 'Há ± 1 mês'
+            : `Há ± ${differenceInMonths} meses`
       } else if (differenceInDays === 0) {
         dateInTextFormat = 'Hoje'
       } else if (differenceInDays === 1) {
@@ -115,11 +135,15 @@ export default {
 
       return dateInTextFormat
     },
+    testa (evento) {
+      console.log(evento)
+    },
     formatDate (date) {
       return moment(date).format('DD/MM/YYYY')
     },
     formatTextoTramitacao (textoTramitacao, key) {
-      return textoTramitacao.length > this.MAX_TEXT_LENGTH && !this.isExpanded(key)
+      return textoTramitacao.length > this.MAX_TEXT_LENGTH &&
+        !this.isExpanded(key)
         ? `${textoTramitacao.substring(0, this.MAX_TEXT_LENGTH - 50)}... (+)`
         : textoTramitacao
     },
@@ -127,11 +151,17 @@ export default {
       if (!this.isExpanded(key)) {
         this.expandedDescriptions.push(key)
       } else {
-        this.expandedDescriptions = this.expandedDescriptions.filter(currentKey => key !== currentKey)
+        this.expandedDescriptions = this.expandedDescriptions.filter(
+          currentKey => key !== currentKey
+        )
       }
     },
     isExpanded (key) {
-      return this.expandedDescriptions.findIndex(currentKey => currentKey === key) !== -1
+      return (
+        this.expandedDescriptions.findIndex(
+          currentKey => currentKey === key
+        ) !== -1
+      )
     }
   },
   watch: {
@@ -144,10 +174,10 @@ export default {
 
 <style scoped>
 .el-collapse {
-  margin-top: 1rem
+  margin-top: 1rem;
 }
 .title {
-  font-size: .97rem;
+  font-size: 0.97rem;
 }
 .eventos-tram {
   font-size: 10pt;
@@ -157,8 +187,9 @@ table {
   border-collapse: collapse;
   width: 100%;
 }
-th, td {
-  padding: .5rem;
+th,
+td {
+  padding: 0.5rem;
   text-align: left;
   vertical-align: top;
 }
