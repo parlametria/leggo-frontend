1a8c3694613bd1601f40c88a782db47d4eee92ef
Jair Guedes <jairguedesferreira@gmail.com>
2019-09-06 09:46:11 -0300

    Lint

--- src/components/Graphics.vue
+++ src/components/Graphics.vue
@@ -1,6 +1,8 @@
 <template>
   <el-col class="graphics-container">
-    <temperature-graphic :temp_historico="prop.lastEtapa.temperatura_historico" :id="prop.id" />
+    <temperature-graphic
+      :temp_historico="prop.lastEtapa.temperatura_historico"
+      :id="prop.id" />
     <temperature-info
       :id="prop.lastEtapa.id_ext"
       :texto="'Temperatura dos últimos 3 meses'"
