1c169b98873bf7d69a7ac556fd2eae07ee3dcc5f
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-09-27 16:38:50 -0300

    Corrige erros de lint.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-const forceSsl = require('force-ssl-heroku');
+const forceSsl = require('force-ssl-heroku')
 
 const app = express()
 
-app.use(forceSsl);
+app.use(forceSsl)
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 const port = process.env.PORT || 8080
