36aac62351dcff70de5c15639b8e0c11ff6cab7d
Juan Barros <juanlyrabarros@gmail.com>
2019-11-13 15:56:19 -0300

    Refatora código para modularizar

--- src/components/card/expanded/rede/InfluenciaGraph.vue
+++ src/components/card/expanded/rede/InfluenciaGraph.vue
@@ -23,6 +23,7 @@ import { vaxios } from "./mocks/vaxios";
 // "@/stores/voz_ativa_axios";
 import Tooltip from "./Tooltip";
 import SelectFilter from "./SelectFilter";
+import legendas from "./mixins/legendas.js"
 
 export default {
   name: "InfluenciaGraph",
@@ -30,6 +31,7 @@ export default {
     Tooltip,
     SelectFilter
   },
+  mixins: [legendas],
   props: {
     id_leggo: {
       type: Number,
@@ -45,9 +47,7 @@ export default {
       edges: [],
       activeNode: null,
       nodeHover: null,
-      filter: "",
-      padding_right: window.innerWidth <= 414 ? 50 : 30,
-      font_size: window.innerWidth <= 414 ? 10 : 6
+      filter: ""
     };
   },
   computed: {
@@ -203,99 +203,10 @@ export default {
         group,
         links,
         vertex,
-        title,
-        width,
-        font_size,
-        padding_right,
-        height
+        title
       } = this;
 
-      const { svg } = this;
-      svg
-        .select(".everything")
-        .append("text")
-        .style("fill", "gray")
-        .text("Governo")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", font_size)
-        .attr("x", width - padding_right)
-        .attr("y", height - 10);
-
-      svg
-        .append("text")
-        .style("fill", "gray")
-        .text("Oposição")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", font_size)
-        .attr("y", height - 10);
-
-      const g = svg
-        .append("g")
-        .attr("transform", "translate(" + (width - 30) + "," + 10 + ")");
-
-      var gradient = svg
-        .append("defs")
-        .append("linearGradient")
-        .attr("id", "gradient")
-        .attr("x1", "0%")
-        .attr("y1", "50%")
-        .attr("x2", "100%")
-        .attr("y2", "50%");
-
-      gradient
-        .append("stop")
-        .attr("offset", "0%")
-        .attr("stop-color", "#D9DCEB")
-        .attr("stop-opacity", 1);
-
-      gradient
-        .append("stop")
-        .attr("offset", "100%")
-        .attr("stop-color", "#3E1D7E")
-        .attr("stop-opacity", 1);
-
-      g.append("rect")
-        .attr("height", 4)
-        .attr("x", -15)
-        .attr("y", 2)
-        .attr("width", 40)
-        .attr("fill", "url(#gradient)");
-
-      g.append("text")
-        .attr("class", "caption")
-        .attr("y", 1)
-        .attr("x", -15)
-        .attr("fill", "#000")
-        .attr("font-size", 3)
-        .attr("text-anchor", "start")
-        .attr("font-weight", "bold")
-        .attr("font-family", "sans-serif")
-        .text("Contribuição do autor");
-
-      g.append("text")
-        .attr("class", "caption")
-        .attr("y", 19)
-        .attr("x", -15)
-        .attr("fill", "#000")
-        .attr("font-size", 3)
-        .attr("text-anchor", "start")
-        .attr("font-weight", "bold")
-        .attr("font-family", "sans-serif")
-        .text("Influência Política");
-
-      var radios = [1, 2, 3, 4, 5];
-      var x0 = -18;
-      var padding = 2;
-
-      g.selectAll("circle")
-        .data(radios)
-        .enter()
-        .append("circle")
-        .attr("fill", "#7566ae")
-        .attr("cx", r => (x0 = x0 + 2 * r + padding))
-        .attr("cy", 25)
-        .attr("r", r => r);
-
+      this.createLegends()
       this.simulation.on("tick", function(d) {
         // position links
         links
@@ -308,10 +219,6 @@ export default {
         vertex.attr("transform", d => `translate(${d.x} , ${d.y})`);
         title.attr("transform", d => `translate(${d.x} , ${d.y})`);
       });
-    },
-    onResize() {
-      this.width = this.$el.offsetWidth;
-      this.height = this.$el.offsetHeight;
     },
     getForceByLength(length) {
       return 1 - length / (length + 20);
