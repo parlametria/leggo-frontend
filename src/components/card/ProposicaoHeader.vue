4f1bfeeeab7c478659f600e5b97a358c9b2671a2
jairnetojp <jair.neto@ccc.ufcg.edu.br>
2019-12-16 14:48:50 -0300

    Deixa a pressão ligada ao id_leggo

--- src/components/card/ProposicaoHeader.vue
+++ src/components/card/ProposicaoHeader.vue
@@ -36,7 +36,7 @@
     />
     <bar
       class="pressao"
-      :ultimo_valor="prop.lastEtapa.ultima_pressao"
+      :ultimo_valor="prop.ultima_pressao"
       :cor="'#FFAB0F'"
       :max_valor="100"
       :tooltip-texto="'Pressão da Semana'"/>
