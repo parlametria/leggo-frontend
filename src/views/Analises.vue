ce1b7690a445e4dde0317c9d8f47510b9b598f74
Jefferson Neves <jefferson.rpn@gmail.com>
2020-07-02 15:23:32 -0300

    Aponta os links para os PDFs reais

--- src/views/Analises.vue
+++ src/views/Analises.vue
@@ -3,13 +3,33 @@
     <h2>Análises</h2>
     <h3>Boletins Congresso Remoto</h3>
     <ul class="links">
-      <li><span class="links-item">Boletim 1</span> <a href="#">O processo legislativo na deliberação remota</a></li>
+      <li>
+        <span class="links-item">Boletim 1</span>
+        <a
+          href="https://drive.google.com/file/d/1dC1nQgS9DFf1VOPZDYRHOhUV3h-aCSQp/view?usp=sharing"
+          target="_blank">O processo legislativo na deliberação remota</a>
+      </li>
     </ul>
     <h3>Congresso Remoto em Notas</h3>
     <ul class="links">
-      <li><span class="links-item">Nota 1</span> <a href="#">O que virou lei na pandemia?</a></li>
-      <li><span class="links-item">Nota 2</span> <a href="#">O quórum do Congresso no período remoto</a></li>
-      <li><span class="links-item">Nota 3</span> <a href="#">Medidas provisórias durante a pandemia</a></li>
+      <li>
+        <span class="links-item">Nota 1</span>
+        <a
+          href="https://drive.google.com/file/d/1fH15K8EUJGiv1yLfQuQxsjJDkN6mE0lL/view?usp=sharing"
+          target="_blank">O que virou lei na pandemia?</a>
+      </li>
+      <li>
+        <span class="links-item">Nota 2</span>
+        <a
+          href="https://drive.google.com/file/d/1XosUBKbnazOMcSZ-5C-AgxJsh0uL88hV/view?usp=sharing"
+          target="_blank">O quórum do Congresso no período remoto</a>
+      </li>
+      <li>
+        <span class="links-item">Nota 3</span>
+        <a
+          href="https://drive.google.com/file/d/1T5q-SLCHu2SXTb1RkeY6_sM9cYIdX9D4/view?usp=sharing"
+          target="_blank">Medidas provisórias durante a pandemia</a>
+      </li>
     </ul>
   </div>
 </template>
