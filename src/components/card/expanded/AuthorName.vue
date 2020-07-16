104f21744b80a0277db34909e7b0042de30d10c9
gileadekelvin <gileadekelvin@gmail.com>
2020-07-16 10:16:03 -0300

    Corrige erros de lint

--- src/components/card/expanded/AuthorName.vue
+++ src/components/card/expanded/AuthorName.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <el-popover width="200" placement="right-start" trigger="hover" :disabled="disabled">
-      <span slot="reference" class="small-text-field small-margin-top">{{ formataAutores() }}</span>
-      <span class="author" v-for="(autor, i) in author" :key="i">
+    <el-popover
+      width="200"
+      placement="right-start"
+      trigger="hover"
+      :disabled="disabled">
+      <span
+        slot="reference"
+        class="small-text-field small-margin-top">{{ formataAutores() }}</span>
+      <span
+        class="author"
+        v-for="(autor, i) in author"
+        :key="i">
         <h5>{{ formataNomeAutor(autor.autor) }}</h5>
       </span>
     </el-popover>
@@ -33,18 +42,18 @@ export default {
     formataAutores: function () {
       if (this.author.length > 1) {
         return this.formataNomeAutor(this.author[0].autor) + ' e ' + (this.author.length - 1) + ' outros'
-        } else {
-          return this.formataNomeAutor(this.author[0].autor)
-        }
+      } else {
+        return this.formataNomeAutor(this.author[0].autor)
+      }
     },
     formataNomeAutor: function (autor) {
       if (autor) {
-        if (autor.is_parlamentar == 1) {
-          let prefix = ""
-          if (autor.casa == "camara") {
-            prefix = "Dep."
-          } else if (autor.casa == "senado") {
-            prefix = "Sen."
+        if (autor.is_parlamentar === 1) {
+          let prefix = ''
+          if (autor.casa === 'camara') {
+            prefix = 'Dep.'
+          } else if (autor.casa === 'senado') {
+            prefix = 'Sen.'
           }
 
           return `${prefix} ${autor.nome} (${autor.partido}/${autor.uf})`
@@ -52,7 +61,7 @@ export default {
           return autor.nome
         }
       } else {
-        return "Autor não identificado"
+        return 'Autor não identificado'
       }
     }
   }
