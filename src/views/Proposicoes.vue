acfa84b952fcf48cf4cab40b98930702800909e4
gileadekelvin <gileadekelvin@gmail.com>
2020-07-21 10:12:35 -0300

    Reposiciona barra de pesquisa

--- src/views/Proposicoes.vue
+++ src/views/Proposicoes.vue
@@ -1,77 +1,78 @@
 <template>
   <div class="content">
     <div class="sessao">
-      <lista-anotacoes
-        :date='dateRef'/>
+      <lista-anotacoes :date="dateRef" />
     </div>
     <div class="sessao">
-      <ultimos-eventos/>
+      <ultimos-eventos />
     </div>
-    <p v-if="pending.proposicoes">Carregando proposições <i class="el-icon-loading"/></p>
+    <p v-if="pending.proposicoes">
+      Carregando proposições
+      <i class="el-icon-loading" />
+    </p>
     <p v-else-if="error.proposicoes">Falha no carregamento</p>
-    <transition
-      v-else
-      name="el-fade-in"
-      mode="out-in">
-      <div v-if="filteredProps.length">
+    <transition v-else name="el-fade-in" mode="out-in">
+      <div>
         <h2>Proposições</h2>
         <filter-button />
-        <div class="session">
-          <header ref="emPautaHeader">
-            <h3 :class="{disabled: emPauta.length === 0}">Na pauta oficial</h3>
-          </header>
-          <div v-if="emPauta.length">
-            <temperature-button />
+        <div v-if="filteredProps.length">
+          <div class="session">
+            <header ref="emPautaHeader">
+              <h3 :class="{disabled: emPauta.length === 0}">Na pauta oficial</h3>
+            </header>
+            <div v-if="emPauta.length">
+              <temperature-button />
+            </div>
+            <div ref="emPautaSession">
+              <proposicao-item
+                :id="prop.id_leggo"
+                :key="prop.id_leggo"
+                v-for="prop in emPauta"
+                :prop="prop"
+              />
+            </div>
           </div>
-          <div ref="emPautaSession">
-            <proposicao-item
-              :id="prop.id_leggo"
-              :key="prop.id_leggo"
-              v-for="prop in emPauta"
-              :prop="prop"/>
+          <div class="session">
+            <header ref="notEmPautaHeader">
+              <h3 :class="{disabled: notEmPauta.length === 0}">Fora da pauta oficial da semana</h3>
+            </header>
+            <div v-if="notEmPauta.length">
+              <temperature-button />
+            </div>
+            <div ref="notEmPautaSession" class="section">
+              <proposicao-item
+                :id="prop.id_leggo"
+                :key="prop.id_leggo"
+                v-for="prop in propPaged"
+                :prop="prop"
+              />
+              <pagination-bar
+                :size="Math.ceil(notEmPauta.length / quantityProp)"
+                :limit="10"
+                :initial="pageNumber"
+                @change="(number) => updatePageNumber(number)"
+              />
+            </div>
           </div>
         </div>
-        <div class="session">
-          <header ref="notEmPautaHeader">
-            <h3 :class="{disabled: notEmPauta.length === 0}">Fora da pauta oficial da semana</h3>
-          </header>
-          <div v-if="notEmPauta.length">
-            <temperature-button />
-          </div>
-          <div
-            ref="notEmPautaSession"
-            class="section">
-            <proposicao-item
-              :id="prop.id_leggo"
-              :key="prop.id_leggo"
-              v-for="prop in propPaged"
-              :prop="prop"/>
-            <pagination-bar
-              :size="Math.ceil(notEmPauta.length / quantityProp)"
-              :limit="10"
-              :initial="pageNumber"
-              @change="(number) => updatePageNumber(number)"
-            />
-          </div>
-        </div>
+        <p v-else>Nenhuma proposição para mostrar...</p>
       </div>
-      <p v-else>Nenhuma proposição para mostrar...</p>
     </transition>
   </div>
 </template>
 
 <script>
-import ProposicaoItem from '@/components/card/ProposicaoItem'
-import UltimosEventos from '@/components/UltimosEventos'
-import { mapState, mapActions, mapGetters, mapMutations } from 'vuex'
-import { removeAcentos } from '@/utils'
-import PaginationBar from '@/components/utils/PaginationBar'
-import FilterButton from '@/components/menu/FilterButton'
-import ListaAnotacoes from '@/components/card/expanded/anotacao/ListaDeAnotacoes'
-import TemperatureButton from '@/components/menu/TemperatureButton'
+import ProposicaoItem from "@/components/card/ProposicaoItem";
+import UltimosEventos from "@/components/UltimosEventos";
+import { mapState, mapActions, mapGetters, mapMutations } from "vuex";
+import { removeAcentos } from "@/utils";
+import PaginationBar from "@/components/utils/PaginationBar";
+import FilterButton from "@/components/menu/FilterButton";
+import ListaAnotacoes from "@/components/card/expanded/anotacao/ListaDeAnotacoes";
+import TemperatureButton from "@/components/menu/TemperatureButton";
 
 export default {
-  name: 'Proposicoes',
+  name: "Proposicoes",
   components: {
     ProposicaoItem,
     UltimosEventos,
@@ -80,118 +81,162 @@ export default {
     ListaAnotacoes,
     TemperatureButton
   },
-  data () {
+  data() {
     return {
       activeNames: [],
       quantityProp: 10
-    }
+    };
   },
   methods: {
-    ...mapActions(['listProposicoes', 'maxTemperatura']),
-    ...mapMutations(['setPageNumber']),
+    ...mapActions(["listProposicoes", "maxTemperatura"]),
+    ...mapMutations(["setPageNumber"]),
 
-    checkCategoricalFilters (prop) {
+    checkCategoricalFilters(prop) {
       return this.filter.filters.every(
-        filter => this.getCurrent[filter].length === 0 || this.filter.current[filter].some(v => {
-          if (prop[filter]) {
-            return prop[filter].includes(v)
-          }
-          return prop.lastEtapa[filter].includes(v)
-        })
-      )
+        filter =>
+          this.getCurrent[filter].length === 0 ||
+          this.filter.current[filter].some(v => {
+            if (prop[filter]) {
+              return prop[filter].includes(v);
+            }
+            return prop.lastEtapa[filter].includes(v);
+          })
+      );
     },
-    checkPautaFilter (prop) {
-      const propId = prop.id
-      const emPauta = this.pautas && this.pautas[propId] && this.pautas[propId].length > 0
+    checkPautaFilter(prop) {
+      const propId = prop.id;
+      const emPauta =
+        this.pautas && this.pautas[propId] && this.pautas[propId].length > 0;
 
-      return (!this.filter.emPautaFilter.some(options => options.status) ? true : emPauta)
+      return !this.filter.emPautaFilter.some(options => options.status)
+        ? true
+        : emPauta;
     },
-    checkStatusFilter (prop) {
-      return this.filter.showFinalizadas.status || prop.status === 'Ativa'
+    checkStatusFilter(prop) {
+      return this.filter.showFinalizadas.status || prop.status === "Ativa";
     },
-    checkApelidoFilter (prop) {
-      const apelido = removeAcentos(prop.sigla.toLowerCase() + prop.apelido.toLowerCase())
-      const filtro = removeAcentos(this.filter.nomeProposicaoFilter.toLowerCase())
-      return apelido.match(filtro)
+    checkApelidoFilter(prop) {
+      const apelido = removeAcentos(
+        prop.sigla.toLowerCase() + prop.apelido.toLowerCase()
+      );
+      const filtro = removeAcentos(
+        this.filter.nomeProposicaoFilter.toLowerCase()
+      );
+      return apelido.match(filtro);
     },
-    checkPropMatchesFilter (prop) {
-      return this.checkCategoricalFilters(prop) &&
+    checkPropMatchesFilter(prop) {
+      return (
+        this.checkCategoricalFilters(prop) &&
         this.checkPautaFilter(prop.lastEtapa)
+      );
     },
-    updateSticky (refHeader, refSession) {
-      if (!refSession || !refHeader) return
+    updateSticky(refHeader, refSession) {
+      if (!refSession || !refHeader) return;
 
       // Faz com que o tamanho da barra seja redimensionado conforme o tamanho da janela
-      refHeader.style.width = `${refSession.getBoundingClientRect().width}px`
+      refHeader.style.width = `${refSession.getBoundingClientRect().width}px`;
 
       if (refSession.getBoundingClientRect().top > 0) {
-        refSession.style.paddingTop = '0px'
-        refHeader.classList.remove('sticky')
+        refSession.style.paddingTop = "0px";
+        refHeader.classList.remove("sticky");
       }
       if (refHeader.getBoundingClientRect().top <= 0) {
-        refSession.style.paddingTop = `${refHeader.getBoundingClientRect().height}px`
-        refHeader.classList.add('sticky')
+        refSession.style.paddingTop = `${
+          refHeader.getBoundingClientRect().height
+        }px`;
+        refHeader.classList.add("sticky");
       }
     },
-    sticky () {
-      this.updateSticky(this.$refs.emPautaHeader, this.$refs.emPautaSession)
-      this.updateSticky(this.$refs.notEmPautaHeader, this.$refs.notEmPautaSession)
+    sticky() {
+      this.updateSticky(this.$refs.emPautaHeader, this.$refs.emPautaSession);
+      this.updateSticky(
+        this.$refs.notEmPautaHeader,
+        this.$refs.notEmPautaSession
+      );
     },
-    updatePageNumber (number) {
-      this.setPageNumber(number)
+    updatePageNumber(number) {
+      this.setPageNumber(number);
     }
   },
   computed: {
-    ...mapGetters(['perFilterOptions', 'formattedDateRef', 'getCurrent', 'getNomeInteresse']),
-    filteredProps () {
+    ...mapGetters([
+      "perFilterOptions",
+      "formattedDateRef",
+      "getCurrent",
+      "getNomeInteresse"
+    ]),
+    filteredProps() {
       // Teste para ver se o obj com os filtros já foi inicializado
       if (Object.keys(this.getCurrent).length) {
-        return this.proposicoes.filter(prop => {
-          return this.checkPropMatchesFilter(prop) &&
-          this.checkStatusFilter(prop.lastEtapa) && this.checkApelidoFilter(prop)
-        }).sort((a, b) => {
-          let idA = a.lastEtapa.id
-          let idB = b.lastEtapa.id
-          let pautaA = this.pautas && this.pautas[idA] !== undefined && this.pautas[idA].length > 0
-          let pautaB = this.pautas && this.pautas[idB] !== undefined && this.pautas[idB].length > 0
-          let n = pautaB - pautaA
-          if (n !== 0) {
-            return n
-          }
+        return this.proposicoes
+          .filter(prop => {
+            return (
+              this.checkPropMatchesFilter(prop) &&
+              this.checkStatusFilter(prop.lastEtapa) &&
+              this.checkApelidoFilter(prop)
+            );
+          })
+          .sort((a, b) => {
+            let idA = a.lastEtapa.id;
+            let idB = b.lastEtapa.id;
+            let pautaA =
+              this.pautas &&
+              this.pautas[idA] !== undefined &&
+              this.pautas[idA].length > 0;
+            let pautaB =
+              this.pautas &&
+              this.pautas[idB] !== undefined &&
+              this.pautas[idB].length > 0;
+            let n = pautaB - pautaA;
+            if (n !== 0) {
+              return n;
+            }
 
-          if (this.temperaturas && this.pressoes) {
-            let tempA = this.temperaturas[a.id_leggo] === undefined ? 0 : this.temperaturas[a.id_leggo]
-            let tempB = this.temperaturas[b.id_leggo] === undefined ? 0 : this.temperaturas[b.id_leggo]
-            let pressaoA = this.pressoes[a.id_leggo] === undefined ? 0 : this.pressoes[a.id_leggo]
-            let pressaoB = this.pressoes[b.id_leggo] === undefined ? 0 : this.pressoes[b.id_leggo]
+            if (this.temperaturas && this.pressoes) {
+              let tempA =
+                this.temperaturas[a.id_leggo] === undefined
+                  ? 0
+                  : this.temperaturas[a.id_leggo];
+              let tempB =
+                this.temperaturas[b.id_leggo] === undefined
+                  ? 0
+                  : this.temperaturas[b.id_leggo];
+              let pressaoA =
+                this.pressoes[a.id_leggo] === undefined
+                  ? 0
+                  : this.pressoes[a.id_leggo];
+              let pressaoB =
+                this.pressoes[b.id_leggo] === undefined
+                  ? 0
+                  : this.pressoes[b.id_leggo];
 
-            if (this.filter.temperatureOrder === 'desc') {
-              let comp = tempB - tempA
-              if (comp !== 0) {
-                return comp
+              if (this.filter.temperatureOrder === "desc") {
+                let comp = tempB - tempA;
+                if (comp !== 0) {
+                  return comp;
+                } else {
+                  return pressaoB - pressaoA;
+                }
               } else {
-                return pressaoB - pressaoA
+                let comp = tempA - tempB;
+                if (comp !== 0) {
+                  return comp;
+                } else {
+                  return pressaoA - pressaoB;
+                }
               }
             } else {
-              let comp = tempA - tempB
-              if (comp !== 0) {
-                return comp
-              } else {
-                return pressaoA - pressaoB
-              }
+              return 0;
             }
-          } else {
-            return 0
-          }
-        })
+          });
       } else {
-        return this.proposicoes
+        return this.proposicoes;
       }
     },
-    propPaged () {
-      const init = this.pageNumber * this.quantityProp
-      const end = (this.pageNumber + 1) * this.quantityProp
-      return this.notEmPauta.slice(init, end)
+    propPaged() {
+      const init = this.pageNumber * this.quantityProp;
+      const end = (this.pageNumber + 1) * this.quantityProp;
+      return this.notEmPauta.slice(init, end);
     },
     ...mapState({
       proposicoes: state => state.proposicoes.proposicoes,
@@ -204,32 +249,38 @@ export default {
       pressoes: state => state.pressao.ultimasPressoes,
       dateRef: state => state.filter.dateRef
     }),
-    emPauta () {
+    emPauta() {
       return this.filteredProps.filter(prop => {
-        const propId = prop.lastEtapa.id
-        return this.pautas && this.pautas[propId] && this.pautas[propId].length > 0
-      })
+        const propId = prop.lastEtapa.id;
+        return (
+          this.pautas && this.pautas[propId] && this.pautas[propId].length > 0
+        );
+      });
     },
-    notEmPauta () {
+    notEmPauta() {
       return this.filteredProps.filter(prop => {
-        const propId = prop.lastEtapa.id
-        return !(this.pautas && this.pautas[propId] && this.pautas[propId].length > 0)
-      })
+        const propId = prop.lastEtapa.id;
+        return !(
+          this.pautas &&
+          this.pautas[propId] &&
+          this.pautas[propId].length > 0
+        );
+      });
     }
   },
-  async mounted () {
-    window.addEventListener('scroll', this.sticky)
-    window.addEventListener('resize', this.sticky)
+  async mounted() {
+    window.addEventListener("scroll", this.sticky);
+    window.addEventListener("resize", this.sticky);
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
 @import "@/base.scss";
 
 .flex {
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 }
 .session {
   position: relative;
